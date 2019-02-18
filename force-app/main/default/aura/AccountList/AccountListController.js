a7263a957f325c6a059db293b5e3017475aef96c
Tidar <tidarharyo@rocketmail.com>
2019-02-18 16:05:42 +0700

    Update AccountList component

--- force-app/main/default/aura/AccountList/AccountListController.js
+++ force-app/main/default/aura/AccountList/AccountListController.js
@@ -1,5 +1,10 @@
 ({
-    myAction : function(component, event, helper) {
+  doInit: function(component, _event) {
+    var action = component.get("c.findAll");
+    action.setCallback(this, function(a) {
+      component.set("v.accounts", a.getReturnValue());
+    });
 
-    }
+    $A.enqueueAction(action);
+  }
 })
