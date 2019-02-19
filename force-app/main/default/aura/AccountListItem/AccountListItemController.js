5185e5d1ed932ed5cd2c7e09371a6f1f1f44d72a
Tidar <tidarharyo@rocketmail.com>
2019-02-19 21:45:28 +0700

    Add SelectedAccounnt event to AccounntListItem componnent

--- force-app/main/default/aura/AccountListItem/AccountListItemController.js
+++ force-app/main/default/aura/AccountListItem/AccountListItemController.js
@@ -1,5 +1,7 @@
 ({
-    myAction : function(component, event, helper) {
-
-    }
+  accountSelected: function(component) {
+    var event = $A.get("e.c:AccountSelected");
+    event.setParams({ "account": component.get("v.account") });
+    event.fire();
+  }
 })
