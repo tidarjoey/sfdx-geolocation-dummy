dd2b231aab9c51adde514903191d01ee4b5c9399
Tidar <tidarharyo@rocketmail.com>
2019-02-19 18:48:52 +0700

    Implement AccountsLoaded in AccountMap component

--- force-app/main/default/aura/AccountMap/AccountMapController.js
+++ force-app/main/default/aura/AccountMap/AccountMapController.js
@@ -3,8 +3,17 @@
     var map = L.map('map', { zoomContol: false }).setView([37.784173, -122.401557], 14);
     L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}',
       {
-        attribution: 'Tiles © Esri'
+        attribution: 'tidarjoey'
       }).addTo(map);
     component.set("v.map", map);
+  },
+  accountsLoaded: function(component, event, _helper) {
+    var map = component.get('v.map');
+    var accounts = event.getParam('accounts');
+    for (var i = 0; i < accounts.length; i++) {
+      var account = accounts[i];
+      var latLng = [account.Location__Latitude__s, account.Location__Longitude__s];
+      L.marker(latLng, { account: account }).addTo(map);
+    }
   }
 })
